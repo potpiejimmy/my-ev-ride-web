9eef8893198ac679c7092e7c25f1c01d3f451b15
potpiejimmy <t.liese@outlook.com>
2017-08-27 09:47:18 +0200

    basic input form validation

--- src/app/pages/mycars.ts
+++ src/app/pages/mycars.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AssetsService } from '../services/api/assets.service';
 import { AppService } from '../services/app.service';
@@ -11,8 +12,10 @@ import { ChooseLocationComponent } from '../components/chooselocation';
 export class MyCarsComponent {
 
   @ViewChild("carlocation")
-  public carLocation: ChooseLocationComponent;
+  carLocation: ChooseLocationComponent;
 
+  editform: FormGroup;
+  
   mycars = [];
 
   car: any = null;
@@ -23,11 +26,25 @@ export class MyCarsComponent {
     {label:'Model 3 White',  value:'tesla_model3_white'}
   ]
 
-  constructor(public app: AppService, private router: Router, private assetsService: AssetsService) {}
+  constructor(public app: AppService,
+    private fb: FormBuilder,
+    private router: Router,
+    private assetsService: AssetsService) {}
 
   ngOnInit() {
+    this.buildEditForm();
     this.refresh();
   }
+
+  private buildEditForm() {
+    this.editform = this.fb.group({
+      'make': new FormControl('', Validators.required),
+      'model': new FormControl('', Validators.required),
+      'image': new FormControl('', Validators.required),
+      'value': new FormControl('', Validators.required),
+      'sharecount': new FormControl('', Validators.required),
+  });
+}
 
   refresh() {
     this.assetsService.getMyCars().then(c => this.mycars = c);
@@ -40,6 +57,11 @@ export class MyCarsComponent {
   }
 
   save() {
+    if (!this.editform.valid) {
+      this.app.setMessage("Form validation failure", "Please enter all required fields", "error");
+      return;
+    }
+
     this.car.lat = this.carLocation.latitude;
     this.car.lon = this.carLocation.longitude;
     this.car.location = this.carLocation.formattedAddressInput;
