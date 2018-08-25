304f165c495df848b4af73a9832fa0db96a5492a
potpiejimmy <t.liese@outlook.com>
2018-08-25 23:11:38 +0200

    basic splash, restored p-cards on main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,81 +8,40 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 import {AppRoutes} from './app.routes';
 import {environment} from '../environments/environment';
 
-
+// PrimeNG
 import {AccordionModule} from 'primeng/primeng';
 import {AutoCompleteModule} from 'primeng/primeng';
-import {BreadcrumbModule} from 'primeng/primeng';
 import {ButtonModule} from 'primeng/primeng';
-import {CalendarModule} from 'primeng/primeng';
 import {CaptchaModule} from 'primeng/primeng';
-import {CarouselModule} from 'primeng/primeng';
-import {ColorPickerModule} from 'primeng/primeng';
-import {ChartModule} from 'primeng/primeng';
 import {CheckboxModule} from 'primeng/primeng';
-import {ChipsModule} from 'primeng/primeng';
-import {CodeHighlighterModule} from 'primeng/primeng';
 import {ConfirmDialogModule} from 'primeng/primeng';
-import {SharedModule} from 'primeng/primeng';
-import {ContextMenuModule} from 'primeng/primeng';
-import {DataGridModule} from 'primeng/primeng';
-import {DataListModule} from 'primeng/primeng';
-import {DataScrollerModule} from 'primeng/primeng';
-import {DataTableModule} from 'primeng/primeng';
-import {DialogModule} from 'primeng/primeng';
-import {DragDropModule} from 'primeng/primeng';
-import {DropdownModule} from 'primeng/primeng';
-import {EditorModule} from 'primeng/primeng';
 import {FieldsetModule} from 'primeng/primeng';
-import {FileUploadModule} from 'primeng/primeng';
-import {GalleriaModule} from 'primeng/primeng';
-import {GMapModule} from 'primeng/primeng';
 import {GrowlModule} from 'primeng/primeng';
 import {InputMaskModule} from 'primeng/primeng';
 import {InputSwitchModule} from 'primeng/primeng';
 import {InputTextModule} from 'primeng/primeng';
 import {InputTextareaModule} from 'primeng/primeng';
-import {LightboxModule} from 'primeng/primeng';
-import {ListboxModule} from 'primeng/primeng';
-import {MegaMenuModule} from 'primeng/primeng';
 import {MenuModule} from 'primeng/primeng';
 import {MenubarModule} from 'primeng/primeng';
 import {MessagesModule} from 'primeng/primeng';
-import {MultiSelectModule} from 'primeng/primeng';
-import {OrderListModule} from 'primeng/primeng';
-import {OrganizationChartModule} from 'primeng/primeng';
-import {OverlayPanelModule} from 'primeng/primeng';
 import {PaginatorModule} from 'primeng/primeng';
 import {PanelModule} from 'primeng/primeng';
-import {PanelMenuModule} from 'primeng/primeng';
 import {PasswordModule} from 'primeng/primeng';
-import {PickListModule} from 'primeng/primeng';
 import {ProgressBarModule} from 'primeng/primeng';
 import {RadioButtonModule} from 'primeng/primeng';
-import {RatingModule} from 'primeng/primeng';
-import {ScheduleModule} from 'primeng/primeng';
 import {ScrollPanelModule} from 'primeng/scrollpanel';
-import {SelectButtonModule} from 'primeng/primeng';
-import {SlideMenuModule} from 'primeng/primeng';
-import {SliderModule} from 'primeng/primeng';
 import {SpinnerModule} from 'primeng/primeng';
-import {SplitButtonModule} from 'primeng/primeng';
-import {StepsModule} from 'primeng/primeng';
+import {DataTableModule} from 'primeng/primeng';
 import {TableModule} from 'primeng/table';
 import {TabMenuModule} from 'primeng/primeng';
-import {TabViewModule} from 'primeng/primeng';
-import {TerminalModule} from 'primeng/primeng';
-import {TieredMenuModule} from 'primeng/primeng';
-import {ToggleButtonModule} from 'primeng/primeng';
 import {ToolbarModule} from 'primeng/primeng';
-import {TooltipModule} from 'primeng/primeng';
-import {TreeModule} from 'primeng/primeng';
-import {TreeTableModule} from 'primeng/primeng';
+import {CardModule} from 'primeng/primeng';
+import {ConfirmationService} from 'primeng/primeng';
 
+// App
 import {AppComponent}  from './app.component';
 import {AppTopbarComponent}  from './components/topbar';
 import {AppFooterComponent}  from './components/footer';
-
-import { ConfirmationService } from 'primeng/primeng';
 
 // app specifics
 import { AgmCoreModule } from '@agm/core';
@@ -115,72 +74,31 @@ import {AboutComponent} from "./pages/about";
         BrowserAnimationsModule,
         AccordionModule,
         AutoCompleteModule,
-        BreadcrumbModule,
         ButtonModule,
-        CalendarModule,
         CaptchaModule,
-        CarouselModule,
-        ColorPickerModule,
-        ChartModule,
         CheckboxModule,
-        ChipsModule,
-        CodeHighlighterModule,
         ConfirmDialogModule,
-        SharedModule,
-        ContextMenuModule,
-        DataGridModule,
-        DataListModule,
-        DataScrollerModule,
-        DataTableModule,
-        DialogModule,
-        DragDropModule,
-        DropdownModule,
-        EditorModule,
         FieldsetModule,
-        FileUploadModule,
-        GalleriaModule,
-        GMapModule,
         GrowlModule,
         InputMaskModule,
         InputSwitchModule,
         InputTextModule,
         InputTextareaModule,
-        LightboxModule,
-        ListboxModule,
-        MegaMenuModule,
         MenuModule,
         MenubarModule,
         MessagesModule,
-        MultiSelectModule,
-        OrderListModule,
-        OrganizationChartModule,
-        OverlayPanelModule,
         PaginatorModule,
         PanelModule,
-        PanelMenuModule,
         PasswordModule,
-        PickListModule,
         ProgressBarModule,
         RadioButtonModule,
-        RatingModule,
-        ScheduleModule,
         ScrollPanelModule,
-        SelectButtonModule,
-        SlideMenuModule,
-        SliderModule,
         SpinnerModule,
-        SplitButtonModule,
-        StepsModule,
+        DataTableModule,
         TableModule,
         TabMenuModule,
-        TabViewModule,
-        TerminalModule,
-        TieredMenuModule,
-        ToggleButtonModule,
         ToolbarModule,
-        TooltipModule,
-        TreeModule,
-        TreeTableModule,
+        CardModule,
         // app specifics
         LocalStorageModule.withConfig({ prefix: 'myevride', storageType: 'localStorage' }), // or sessionStorage
         AgmCoreModule.forRoot({
