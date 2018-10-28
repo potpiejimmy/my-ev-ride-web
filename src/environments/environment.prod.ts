a6ba5022fdd764acf771d7deaab486e707acdd8c
potpiejimmy <t.liese@outlook.com>
2018-10-28 09:18:01 +0100

    upgraded to Angular 7, fixed how-it-works link.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  landingUrl: 'https://www.thliese.net',
   apiUrl: 'https://doogetha.com/myevride/api/',
   captchaSiteKey: '6LfzPzEUAAAAAM18P0lPawg416OPTz4AQd-1YY4B'
 };
