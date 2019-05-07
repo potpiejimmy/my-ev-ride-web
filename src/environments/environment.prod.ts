36a280746bb05dacf363af1468cb2bfec4a1fb31
potpiejimmy <t.liese@outlook.com>
2019-05-07 10:53:04 +0200

    configured prod for stage environment on stage*.my-ev-ride.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  landingUrl: 'https://www.thliese.net',
-  apiUrl: 'https://doogetha.com/myevride/api/',
+  landingUrl: 'https://stage.my-ev-ride.com',
+  apiUrl: 'https://stageapi.my-ev-ride.com/myevride/api/',
   captchaSiteKey: '6LfzPzEUAAAAAM18P0lPawg416OPTz4AQd-1YY4B'
 };
